5e8bc25002f2928010ab6ad1847d53128078bd84
rwabusini <rwabusini@gmail.com>
2020-07-14 21:29:09 +0300

    DropDown list

--- client/App.tsx
+++ client/App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Home from './Home'
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello Android :D </Text>
       <StatusBar style="auto" />
+      <Home/>
     </View>
   );
 }
