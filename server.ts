cd0ebd2f5109da76e925b03e1550ff47880ae4d6
Yosef-Mag <yosef.maghaireh@gmail.com>
2020-07-14 10:59:23 -0700

    DataBase connection on atlas/user&nany schema

--- server.ts
+++ server.ts
@@ -1,13 +1,26 @@
 var express = require('express');
 var app = express();
-var port = 5000
+var mongoose = require('mongoose');
+var port = process.env.PORT || 5000
+
+require('dotenv').config(); // to read .env file
+
+const mongoURI = process.env.ATLAS_URI;
 
 
+// test get req
 app.get('/', function (req:any, res:any) {
     res.send('Hello World')
-  })
+});
+
+// creat a connction to database on atlas
+mongoose
+    .connect(mongoURI, {useNewUrlParser: true})
+    .then(() => console.log('DataBase connected to the server'))
+    .catch((err:any) => console.log(err))
 
 
+// starting the server
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
