28ea36a8733957157d6fc119671f92ff998f26f2
rwabusini <rwabusini@gmail.com>
2020-07-15 17:20:08 +0300

    component folder added

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './Home'
+import Home from './components/Home'
 export default function App() {
   return (
     <View style={styles.container}>
