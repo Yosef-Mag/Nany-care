b8735656322db05c812f66f7987b49dd5ee4f2c7
Afnan shawawreh <64199018+AfnanShawawreh@users.noreply.github.com>
2020-07-16 07:09:14 +0300

    <Routes />

--- frontend/App.js
+++ frontend/App.js
@@ -1,22 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Home from "./components/Home";
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Home />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+import React, { Component } from "react";
+import { AppRegistry, View } from "react-native";
+import Routes from "./components/Routes.js";
+
+class NanyApp extends Component {
+  render() {
+    return <Routes />;
+  }
+}
+export default NanyApp;
+AppRegistry.registerComponent("NanyApp", () => NanyApp);
