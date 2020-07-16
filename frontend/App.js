7c19d621ae403ba5e28b110ea72f6858b1fcb79b
suraaloqaily <aloqailysura@gmail.com>
2020-07-16 13:54:27 +0300

    connect profile / Home / Log out in slidbar

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import MyDrawer from "./slidbar";
+import MyDrawer from "./components/slidbar";
 import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
@@ -12,25 +10,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-=======
-
-import React, { Component } from "react";
-import { AppRegistry, View } from "react-native";
-import Routes from "./components/Routes.js";
-
-class NanyApp extends Component {
-  render() {
-    return <Routes />;
-  }
-}
-export default NanyApp;
-AppRegistry.registerComponent("NanyApp", () => NanyApp);
->>>>>>> 29cdc1d2f362e7cf84998e1e80b05f6840faa769
