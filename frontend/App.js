eaf314e120494c43f4a3a94c00523ddf6c8c14a6
rwabusini <rwabusini@gmail.com>
2020-07-16 10:06:10 +0300

    getting nany's from db  method in progress

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './components/Home'
+import * as Home from './components/Home'
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Home/>
+      <Home.place/>
+      <Home.educationLevel/>
+      <Home.howManyKidsCanHandle/>
     </View>
   );
 }
@@ -19,4 +22,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,35 +1,59 @@
 import React, { useState } from "react";
-import { View, Picker, StyleSheet } from "react-native";
+import { View, Picker, StyleSheet, NativeModules } from "react-native";
 
-const Home = () => {
-  const [selectedValue, setSelectedValue] = useState("java");
+
+
+// dropdown list for the place
+export function place () {
+  const [selectedValue, setSelectedValue] = useState(" Choose the nany's place");
   return (
-    <View style={styles.container}>
-        
+    <View style={styles.container}> 
       <Picker
         selectedValue={selectedValue}
         style={{ height: 50, width: 150 }}
         onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
       >  
-        <Picker.Item label="3" value="java" />
-        <Picker.Item label="Kids can handle" value="js" />
-        <Picker.Item label="JavaScript" value="js" />
-        <Picker.Item label="JavaScript" value="js" />
-      </Picker>
+        <Picker.Item label="Amman" value="amman" />
+      </Picker> 
+      </View>
+       )}
 
+// dropdown list for the number of Kids Can Handle
+export function howManyKidsCanHandle () {
+  const [selectedValue, setSelectedValue] = useState(" For how many kids" );
+        return (
+     <View style={styles.container}>
       <Picker
         selectedValue={selectedValue}
         style={{ height: 50, width: 150 }}
         onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
       >
-        <Picker.Item label="Name" value="java" />
-        <Picker.Item label="Kids can handle" value="js" />
-        <Picker.Item label="JavaScript" value="js" />
-        <Picker.Item label="JavaScript" value="js" />
+        <Picker.Item label="1" value="1" />
+        <Picker.Item label="2" value="2" />
+        <Picker.Item label="3" value="3" />
+        <Picker.Item label="4" value="4" />
       </Picker>
     </View>
-  );
-}
+        )}
+
+// dropdown list for the Education Level
+export function educationLevel() {
+          const [selectedValue, setSelectedValue] = useState("Choose Nany's education level");
+          return (
+       <View style={styles.container}>
+        <Picker
+          selectedValue={selectedValue}
+          style={{ height: 50, width: 150 }}
+          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+        >
+          <Picker.Item label="college" value="college" />
+          <Picker.Item label="high school" value="high school" />
+        </Picker>
+      </View>
+          )}
+
+
+  
 
 const styles = StyleSheet.create({
   container: {
@@ -39,4 +63,5 @@ const styles = StyleSheet.create({
   }
 });
 
-export default Home;
+
+
