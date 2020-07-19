8b600aa1690f80512bdef68e1f3e0baef64fd31e
suraaloqaily <aloqailysura@gmail.com>
2020-07-19 16:33:17 +0300

    render user profile need test

--- frontend/App.js
+++ frontend/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import MyDrawer from "./components/slidbar";
 import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
@@ -9,12 +8,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-=======
-
-import SignUp from "./components/signup";
-export default function App() {
-  return <SignUp />;
-}
-
-//up to date
->>>>>>> 7a2596eca202138d0bfd93473dcf88b3401a3dbf
