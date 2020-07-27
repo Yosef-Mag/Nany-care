8f6e3bf5e0cb783f77341bceb34f7e6fca324601
rwabusini <rwabusini@gmail.com>
2020-07-27 20:55:14 +0300

    sending user location to the nanny by sms successful

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,17 @@
-
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-
 var items = require("./models/user");
-//var config = require("./config");
+var config = require("./config");
 var Nany = items.Nany;
 var User = items.User;
 var User = items.User;
-// var accountSid = config.accountSid; // Your Account SID from www.twilio.com/console
-// var authToken =  config.authToken;   // Your Auth Token from www.twilio.com/console
-// var toNum=config.toNum
-// var fromNum=config.fromNum
-//var twilio = require('twilio');
-//var client = new twilio(accountSid, authToken);
-
+var accountSid = config.accountSid; // Your Account SID from www.twilio.com/console
+var authToken = config.authToken; // Your Auth Token from www.twilio.com/console
+var toNum = config.toNum;
+var fromNum = config.fromNum;
+var twilio = require("twilio");
+var client = new twilio(accountSid, authToken);
 const cors = require("cors");
 const router = express.Router();
 var Nannyhandlers = require("./handlers/Nannyhandlers");
@@ -26,58 +23,41 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var port = process.env.PORT || 5000;
-
 require("dotenv").config(); // to read .env file
-
 // test get req
 app.get("/", function (req, res) {
-  console.log(config);
-  res.send("server is a go!");
+  console.log(req);
+  res.send(req);
 });
-
 app.post("/HiringForm", Nannyhandlers.HiringForm);
-
-app.post("/select", userhandlers.selectLocation);
-
+// app.post("/select", userhandlers.selectLocation);
 app.post("/signup", userhandlers.userSignUp);
 app.get("/logout", userhandlers.userLogOut);
 app.post("/login", userhandlers.userLogIn);
 app.get("/ret", Nannyhandlers.retrieve);
-
 app.get("/profile", userhandlers.retriveUserByEmail);
-
 app.get("/profilee", userhandlers.retriveUserByToken);
 app.post("/api/doPayment/", userhandlers.payment);
-
 app.post("/insert", Adminhandlers.insert);
-
 app.post("/adminSignUp", Adminhandlers.adminSignUp);
 app.post("/reserve", Nannyhandlers.reserve);
 app.post("/adminLogIn", Adminhandlers.adminLogIn);
-app.post("/sendSMS", function(req, res) {
- 
-client.messages.create({
-  body: 'Hi from sura i send this message using Node js ',
-  to: toNum ,  // Text this number
-  from: fromNum // From a valid Twilio number
-})
-.then((message) => console.log(message))
-.catch((err)=>console.log(err));
-
- 
+app.post("/sendSMS", function (req, res) {
+  console.log("hi from send sms");
+  var location = req.body;
+  client.messages
+    .create({
+      body:
+        "Hi from Nanny app you have been reserved by a new mommy and this is the location, https://maps.google.com/?ll=" +
+        location.latitude +
+        "," +
+        location.longitude,
+      to: toNum, // Text this number
+      from: fromNum, // From a valid Twilio number
+    })
+    .then((message) => console.log(message))
+    .catch((err) => console.log(err));
 });
-
-app.post("/select" , function(req, res) { 
-  client.messages.create({
-    body: 'Hi from sura i send this message using Node js ',
-    to: toNum ,  // Text this number
-    from: fromNum // From a valid Twilio number
-  })
-  .then((message) => console.log(message))
-  .catch((err)=>console.log(err)); 
-  });
-
-
 app.get("/retrieveAllNanies", Adminhandlers.retriveAllNanies);
 app.delete("/deleteSpecificNany", Adminhandlers.deleteSpecificNany);
 app.patch("/updateNanyInformation", Adminhandlers.updateNanyInformation);
