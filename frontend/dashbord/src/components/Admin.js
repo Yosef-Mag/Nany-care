4f7b4ffe1c783878d68f4423036eb8a5dd0695ad
rwabusini <rwabusini@gmail.com>
2020-07-29 20:02:56 +0300

    fetching nanny data and rendering are done

--- frontend/dashbord/src/components/Admin.js
+++ frontend/dashbord/src/components/Admin.js
@@ -1,14 +1,69 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";   
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+export default function AllNanny() { 
+
+const [nannylist, setNannylist] = useState([]); // state to hold all nanny records data
+useEffect(() => {
+    fetch(`http://192.168.127.74:5000/Admin`)
+      .then(res => res.json())
+      .then(response => {
+        setNannylist(response);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+console.log(nannylist)
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '36ch',
+    backgroundColor: theme.palette.background.paper,
+  },
+  inline: {
+    display: 'inline',
+  },
+}));
 
 
+  const classes = useStyles();
 
-export default function AllNany(prpos) {
-
-    return (
-        <>
-       <div>
-        <h1>Hi</h1>
-       </div>
-        </>
-    )
+  return (
+      <>
+    { nannylist.map((nany) => (
+    <List className={classes.root}>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt={nany.name} src= {nany.image} />
+        </ListItemAvatar>
+        <ListItemText
+          primary= {nany.name}
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary"
+              >
+                {nany.place}
+              </Typography>
+              {" — I'll be in your neighborhood doing errands this…"}
+            </React.Fragment>
+          }
+        /> 
+        <button> Delete </button> <br></br> <br></br>
+        <button> Update </button>
+        </ListItem> 
+    </List>
+  ))
+ }
+ </>
+ ) 
 }
