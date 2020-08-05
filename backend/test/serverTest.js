c3b06130715ff275c150cbb6e74775cd903e4227
nosibah93 <hefa.elayan@gmail.com>
2020-08-05 22:03:45 +0300

    test

--- backend/test/serverTest.js
+++ backend/test/serverTest.js
@@ -11,27 +11,62 @@ chai.use(chaiHttp)
   * Test the /ret endpoint
   */
  describe('/GET test', () => {
-  it(' test ret endpoint', (done) => {
+  it(' test ret endpoint - home page', (done) => {
     chai.request(server)
         .get('/ret')
         .end((err, res) => {
-          res.status.should.be.equal(200);
-          //console.log(res.body)
+          res.status.should.be.to.equal(200);
+          res.body.should.be.an('array')
+          // console.log(res.body)
+
           done();
         });
   });
 });
 
-// describe('/map test', () => {
-//   it('it should test map endpoint', (done) => {
+/*
+  * Test the /profile endpoint
+  */
+
+ describe('/GET test ', () => {
+  it(' test profile endpoint - profile page', (done) => {
+    chai.request(server)
+        .get('/profile')
+        .end((err, res) => {
+          res.status.should.be.to.equal(500);
+          res.body.should.be.an('object')
+          //console.log(res.status)
+
+          done();
+        });
+  });
+});
+
+/*
+  * Test the /POST route
+  */
+//  describe('/POST user', () => {
+//   it('it should not POST a new user', (done) => {
+//       let user = {
+//           name: "hala",
+//           email: "hello@hi.com",
+//           password: 1234567,
+//           phoneNumber: 071243
+//       }
 //     chai.request(server)
-//         .post('/select')
+//         .post('/signup')
+//         .send(user)
 //         .end((err, res) => {
-//           res.status.should.be.equal(200);
-//           //console.log(res.body)
-//               //  res.body.should.be.a([]);
-//               // res.body.length.should.be.eql(0);
+//           //console.log(res.body.name)
+//                res.should.have.status(201);
+//               res.body.should.be.a('object');
+//               //res.body.should.have.property('errors');
+//              // res.body.errors.should.have.property('name');
+//              // res.body.errors.pages.should.have.property('email').eql('required');
 //           done();
 //         });
 //   });
+
 // });
+
+
